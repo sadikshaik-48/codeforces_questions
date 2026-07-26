76718a3fcc49652d625a548d74757b737b70451f
sadikshaik <jafarsadikshaik.48@gmail.com>
2026-07-26 17:22:26 +0530

    adding files

---
+++ Level_A/Sadik.java
@@ -0,0 +1,7 @@
+package Level_A;
+
+public class Sadik {
+    public static void main(String[] args) {
+        System.out.println("Testing whether repo is able to push files through IntelliJ or not");
+    }
+}
