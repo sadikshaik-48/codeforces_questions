da2506481f1ab8f4f78a0e4b226d27cbd165e462
sadikshaik <jafarsadikshaik.48@gmail.com>
2026-07-27 12:25:23 +0530

    Adding 69A solution to the repo

--- Level_A/YoungPhysicist.java
+++ Level_A/YoungPhysicist.java
@@ -5,11 +5,29 @@ import java.util.Scanner;
 public class YoungPhysicist {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-       int[][] arr = new int[n][n];
-        for (int i = 0; i <= n; i++){
 
+        int n = sc.nextInt();
+        int[][] arr = new int[n][3];
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < 3; j++) {
+                arr[i][j] = sc.nextInt();
+            }
         }
 
+        int x = 0, y = 0, z = 0;
+
+        // To add every column
+        for (int i = 0; i < n; i++) {
+            x += arr[i][0];
+            y += arr[i][1];
+            z += arr[i][2];
+        }
+
+        if (x == 0 && y == 0 && z == 0) {
+            System.out.println("YES");
+        } else {
+            System.out.println("NO");
+        }
     }
 }
