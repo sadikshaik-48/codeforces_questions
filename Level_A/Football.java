9875f69748ac11f1ffef57d9870e8e7af1147beb
sadikshaik <jafarsadikshaik.48@gmail.com>
2026-08-02 10:11:16 +0530

    Adding 96A

---
+++ Level_A/Football.java
@@ -0,0 +1,19 @@
+package Level_A;
+
+import java.util.Scanner;
+
+public class Football {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String s = sc.next();
+
+        if (s.contains("0000000") || s.contains("1111111")){
+            System.out.println("YES");
+        }
+        else {
+            System.out.println("NO");
+        }
+    }
+}
+//96
